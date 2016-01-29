11f7ee3d856f82a5e0e5f3e4fe6db6dae23f1830
Robbie Coomber <robbie.coomber@gmail.com>
2016-01-29 11:17:27 +0000

    Move the top level React class into a shared file
    Previously it lived in the index.android.js file, but now I need to reference it from index.os.js

---
+++ app.js
@@ -0,0 +1,71 @@
+'use strict';
+import React, {
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  Navigator
+} from 'react-native';
+import { Provider } from 'react-redux';
+
+import {
+  Splash,
+  WhoAreYou,
+  YourNameEntry
+} from './pages/pages'
+
+import { createDataStore } from './dataStore/dataStore';
+
+export default class TeaAndPronouns extends Component {
+
+  constructor() {
+    super();
+    this.store = null;
+    this.state = {hasStore: false};
+
+    createDataStore()
+      .then((store) => {
+        this.store = store;
+        this.setState({hasStore: true});
+      });
+  }
+
+  render() {
+    if (!this.state.hasStore) {
+      return <Splash/>
+    } else {
+      return (
+        <Provider store={this.store}>
+          <Navigator
+            initialRoute={{id: 'WhoAreYou', name: 'WhoAreYou'}}
+            renderScene={this.renderScene.bind(this)}
+            configureScene={(route) => {
+              if (route.sceneConfig) {
+                return route.sceneConfig;
+              }
+              return Navigator.SceneConfigs.VerticalDownSwipeJump;
+            }}
+          />
+        </Provider>
+      );
+    }
+  }
+
+  renderScene(route, navigator) {
+    if (this.state.hasStore) {
+      var routeId = route.id;
+
+      const props = {
+        navigator: navigator
+      };
+
+      if (routeId === 'WhoAreYou') {
+        return <WhoAreYou {...props}/>;
+      } else if (routeId === 'YourNameEntry') {
+        return <YourNameEntry {...props}/>
+      }
+    } else {
+      return <Splash/>;
+    }
+  }
+}
--- index.android.js
+++ index.android.js
@@ -1,74 +1,8 @@
 'use strict';
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View,
-  Navigator
-} from 'react-native';
-import { Provider } from 'react-redux';
-
 import {
-  Splash,
-  WhoAreYou,
-  YourNameEntry
-} from './pages/pages'
+  AppRegistry
+} from 'react-native';
 
-import { createDataStore } from './dataStore/dataStore';
-
-class TeaAndPronouns extends Component {
-
-  constructor() {
-    super();
-    this.store = null;
-    this.state = {hasStore: false};
-
-    createDataStore()
-      .then((store) => {
-        this.store = store;
-        this.setState({hasStore: true});
-      });
-  }
-
-  render() {
-    if (!this.state.hasStore) {
-      return <Splash/>
-    } else {
-      return (
-        <Provider store={this.store}>
-          <Navigator
-            initialRoute={{id: 'WhoAreYou', name: 'WhoAreYou'}}
-            renderScene={this.renderScene.bind(this)}
-            configureScene={(route) => {
-              if (route.sceneConfig) {
-                return route.sceneConfig;
-              }
-              return Navigator.SceneConfigs.VerticalDownSwipeJump;
-            }}
-          />
-        </Provider>
-      );
-    }
-  }
-
-  renderScene(route, navigator) {
-    if (this.state.hasStore) {
-      var routeId = route.id;
-
-      const props = {
-        navigator: navigator
-      };
-
-      if (routeId === 'WhoAreYou') {
-        return <WhoAreYou {...props}/>;
-      } else if (routeId === 'YourNameEntry') {
-        return <YourNameEntry {...props}/>
-      }
-    } else {
-      return <Splash/>;
-    }
-  }
-}
+import TeaAndPronouns from './app';
 
 AppRegistry.registerComponent('TeaAndPronouns', () => TeaAndPronouns);
